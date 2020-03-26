a774b55803161359f3019a69790260f9c19069ea
Maëlick Claes <himself@maelick.net>
2020-03-26 17:36:13 +0200

    Covid19 Discovery Clients configured

--- codereuse/covid19-data-service/src/main/java/fi/oulu/softwareconstruction/covid19/dataservice/Covid19DataServiceApplication.java
+++ codereuse/covid19-data-service/src/main/java/fi/oulu/softwareconstruction/covid19/dataservice/Covid19DataServiceApplication.java
@@ -2,8 +2,10 @@ package fi.oulu.softwareconstruction.covid19.dataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Covid19DataServiceApplication {
 
 	public static void main(String[] args) {
--- codereuse/covid19-nordic-service/src/main/java/fi/oulu/softwareconstruction/covid19/nordicservice/Covid19NordicServiceApplication.java
+++ codereuse/covid19-nordic-service/src/main/java/fi/oulu/softwareconstruction/covid19/nordicservice/Covid19NordicServiceApplication.java
@@ -2,8 +2,10 @@ package fi.oulu.softwareconstruction.covid19.nordicservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Covid19NordicServiceApplication {
 
 	public static void main(String[] args) {
