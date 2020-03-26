892f49e47f119145124e36303c0b236d77dccd5f
Maëlick Claes <himself@maelick.net>
2020-03-26 14:44:19 +0200

    Covid19 Country Service added

---
+++ codereuse/covid19-country-service/src/main/java/fi/oulu/softwareconstruction/covid19/countryservice/Covid19CountryServiceApplication.java
@@ -0,0 +1,13 @@
+package fi.oulu.softwareconstruction.covid19.countryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19CountryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19CountryServiceApplication.class, args);
+	}
+
+}
---
+++ codereuse/covid19-country-service/src/test/java/fi/oulu/softwareconstruction/covid19/countryservice/Covid19CountryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.oulu.softwareconstruction.covid19.countryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19CountryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
