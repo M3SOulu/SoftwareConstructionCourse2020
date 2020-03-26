d29e783288259f0e08e6eeedce10fdc2f9b09759
Maëlick Claes <himself@maelick.net>
2020-03-26 14:44:54 +0200

    Covid19 Nordic Service added

---
+++ codereuse/covid19-nordic-service/src/main/java/fi/oulu/softwareconstruction/covid19/nordicservice/Covid19NordicServiceApplication.java
@@ -0,0 +1,13 @@
+package fi.oulu.softwareconstruction.covid19.nordicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19NordicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19NordicServiceApplication.class, args);
+	}
+
+}
---
+++ codereuse/covid19-nordic-service/src/test/java/fi/oulu/softwareconstruction/covid19/nordicservice/Covid19NordicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.oulu.softwareconstruction.covid19.nordicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19NordicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
