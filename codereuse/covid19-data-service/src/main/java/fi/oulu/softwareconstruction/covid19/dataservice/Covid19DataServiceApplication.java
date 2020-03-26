369fde0a0619c39f6463142dec944cf4a272bf6e
Maëlick Claes <himself@maelick.net>
2020-03-26 14:44:37 +0200

    Covid19 Data Service added

---
+++ codereuse/covid19-data-service/src/main/java/fi/oulu/softwareconstruction/covid19/dataservice/Covid19DataServiceApplication.java
@@ -0,0 +1,13 @@
+package fi.oulu.softwareconstruction.covid19.dataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19DataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19DataServiceApplication.class, args);
+	}
+
+}
---
+++ codereuse/covid19-data-service/src/test/java/fi/oulu/softwareconstruction/covid19/dataservice/Covid19DataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.oulu.softwareconstruction.covid19.dataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19DataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
